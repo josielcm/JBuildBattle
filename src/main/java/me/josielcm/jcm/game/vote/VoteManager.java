c57ad48325a84d86d36c35f6ae9d76181a8418b9
JosielIANashe <145178093+JosielIANashe@users.noreply.github.com>
2025-05-17 21:48:28 +0000

    Update vote duration in VoteManager from 30 to 180 seconds for gameplay adjustment

--- src/main/java/me/josielcm/jcm/game/vote/VoteManager.java
+++ src/main/java/me/josielcm/jcm/game/vote/VoteManager.java
@@ -61,7 +61,7 @@ public class VoteManager {
         }
 
         openVoteMenu();
-        voteTask = new VoteTask(30, voteMessage);
+        voteTask = new VoteTask(180, voteMessage);
         voteTask.runTaskTimer(JBuildBattle.getInstance(), 0, 20);
         // PlayerManager.playSound(Sound.BLOCK_NOTE_BLOCK_PLING, 1f, 1f);
     }
